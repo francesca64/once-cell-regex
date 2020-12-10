fb18b033681dffa5978ac798cf77413c156d77a3
Francesca Lovebloom <francesca@brainiumstudios.com>
2020-12-10 11:39:07 -0800

    Add byte macros

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+# Version 0.2.1 (2020-12-10)
+
+- Added `byte_regex` and `byte_regex_multi_line` macros.
+
 # Version 0.2.0 (2020-11-04)
 
 - The macros once again only accepts literals, since as it turns out, accepting expressions is a big footgun.
--- src/lib.rs
+++ src/lib.rs
@@ -26,25 +26,52 @@ macro_rules! regex_multi_line {
     }};
 }
 
+#[macro_export]
+macro_rules! byte_regex {
+    ($re:expr $(,)?) => {{
+        static RE: $crate::exports::once_cell::sync::OnceCell<
+            $crate::exports::regex::bytes::Regex,
+        > = $crate::exports::once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| $crate::exports::regex::bytes::Regex::new($re).unwrap())
+    }};
+}
+
+#[macro_export]
+macro_rules! byte_regex_multi_line {
+    ($re:expr $(,)?) => {{
+        static RE: $crate::exports::once_cell::sync::OnceCell<
+            $crate::exports::regex::bytes::Regex,
+        > = $crate::exports::once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| {
+            $crate::exports::regex::bytes::RegexBuilder::new($re)
+                .multi_line(true)
+                .build()
+                .unwrap()
+        })
+    }};
+}
+
 #[cfg(test)]
 mod tests {
-    use regex::Regex;
+    use regex::{bytes, Regex};
 
     #[test]
-    fn regex_macro_lit() {
+    fn regex_macro() {
         let _: &Regex = regex!(r"\w?");
     }
 
     #[test]
-    fn regex_macro_expr() {
-        let n = 5;
-        let re: &Regex = regex!(&format!(r"\w{{{}}}", n));
-        let s = "hotdogs";
-        assert_eq!(re.find(s).map(|m| m.as_str()), Some(&s[0..n]));
+    fn regex_multi_line_macro() {
+        let _: &Regex = regex_multi_line!(r"\w?");
     }
 
     #[test]
-    fn regex_multi_line_macro_lit() {
-        let _: &Regex = regex_multi_line!(r"\w?");
+    fn byte_regex_macro() {
+        let _: &bytes::Regex = byte_regex!(r"\w?");
+    }
+
+    #[test]
+    fn byte_regex_multi_line_macro() {
+        let _: &bytes::Regex = byte_regex_multi_line!(r"\w?");
     }
 }
